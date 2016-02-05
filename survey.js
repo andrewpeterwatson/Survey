ca380a48a9bd082811ab3bbd4502adaac4f4eef8
Andrew Watson <andrewpeterwatson@gmail.com>
2016-02-04 16:22:07 -0800

    work on functionality

--- survey.js
+++ survey.js
@@ -1,13 +1,7 @@
 function Image(call,url) {
   this.call = call;
-  this.url =url;
-};
-randomNum = function(min, max) {
-  return Math.random() * (max - min) - 1;
-};
-randomPic = function() {
-  var randomPicNum = Math.floor(randomNum(1,images.length));
-  randomImage = images[randomPicNum].url;
+  this.url = url;
+  this.chosen = 0;
 };
 var bag = new Image("bag","media/images/png/bag.png");
 var banana = new Image("banana","media/images/png/banana.png");
@@ -21,7 +15,7 @@ var shark = new Image("shark","media/images/png/shark.png");
 var sweep = new Image("sweep","media/images/png/sweep.png");
 var unicorn = new Image("unicorn","media/images/png/unicorn.png");
 var usb = new Image("usb","media/images/png/usb.png");
-var waterCan = new Image("waterCan","media/images/png/waterCan.png");
+var waterCan = new Image("waterCan","media/images/png/water_can.png");
 var wine = new Image("wine","media/images/png/wine.png");
 var images = [bag, banana, boots, chair, cthulhu, dragon, pen, scissors, shark, sweep, unicorn, usb, waterCan, wine];
 
@@ -30,13 +24,30 @@ var images1El = document.getElementById("pic1");
 var images2El = document.getElementById("pic2");
 var images3El = document.getElementById("pic3");
 var imageEl = [images1El,images2El,images3El];
+var pickedPics = [];
 
-  render = function() {
-    for (pics in imageEl) {
+randomNum = function(min, max) {
+  return Math.random() * (max - min) + 1;
+};
+randomPic = function() {
+  var randomPicNum = Math.floor(randomNum(1,images.length));
+  images[randomPicNum].chosen++;
+  pickedPics.push(randomPicNum);
+  console.log(pickedPics);
+    if (randomPicNum == pickedPics) {
       randomPic();
-      var imgEl = document.createElement("img");
-      imgEl.src = randomImage;
-      imageEl[pics].appendChild(imgEl);
-    }
+    } else {
+        randomImage = images[randomPicNum].url;
+      }
+};
+render = function() {
+  for (pics in imageEl) {
+    var imgEl = document.createElement("img");
+    randomPic();
+    imgEl.src = randomImage;
+    console.log(randomImage);
+    imageEl[pics].appendChild(imgEl);
+  }
 };
 render();
+// pickedPics = [];

7837d182eacfa6348e760f6de2a4de6328151958
Andrew Watson <andrewpeterwatson@gmail.com>
2016-02-06 08:43:47 -0800

    more work on loop

--- survey.js
+++ survey.js
@@ -14,35 +14,43 @@ populate = function() {
 randomNum = function(min, max) {
   return Math.floor(Math.random() * (images.length - 1) + 1);
 };
-populate();
-createThree = function() {
-  var used = [];
-  newProd = [];
-  picEl1 = document.getElementById("pic1");
-  picEl2 = document.getElementById("pic2");
-  picEl3 = document.getElementById("pic3");
-  for (var i = 0; i < 3; i++) {
-    randomProd = newProducts[randomNum()];
-    used.push(randomProd);
-    while (used.indexOf(randomProd) == -1) {
-    }
-    newProd.push(randomProd)
-    used.push(randomProd);
+tracker = function() {
+  for (prods in images) {
+    populate();
+    createThree = function() {
+      used = [];
+      notUsed = [];
+      newProd = [];
+      picEl1 = document.getElementById("pic1");
+      picEl2 = document.getElementById("pic2");
+      picEl3 = document.getElementById("pic3");
+        for (var i = 0; i < 3; i++) {
+          newProd.push(newProducts[randomNum()]);
+          // console.log(newProd);
+            // while (used.includes(newProd) === false) {
+            //   used.push(newProd);
+            //   console.log("FAIL");
+        // }
+      }
+      picEl1.src = newProd[0].url;
+      picEl2.src = newProd[1].url;
+      picEl3.src = newProd[2].url;
+    };
+    createThree();
+    var selectImage1 = document.getElementById("pic1");
+    var selectImage2 = document.getElementById("pic2");
+    var selectImage3 = document.getElementById("pic3");
+    selectImage1.addEventListener("click", function(){
+      createThree();
+    });
+    selectImage2.addEventListener("click", function(){
+      createThree();
+    });
+    selectImage3.addEventListener("click", function(){
+      createThree();
+    });
   }
-  picEl1.src = newProd[0].url;
-  picEl2.src = newProd[1].url;
-  picEl3.src = newProd[2].url;
+  selectImage1.removeEventListener("click", function(){})
+  selectImage2.removeEventListener("click", function(){})
+  selectImage3.removeEventListener("click", function(){})
 };
-createThree();
-var selectImage1 = document.getElementById("pic1");
-var selectImage2 = document.getElementById("pic2");
-var selectImage3 = document.getElementById("pic3");
-selectImage1.addEventListener("click", function(){
-  createThree();
-});
-selectImage2.addEventListener("click", function(){
-  createThree();
-});
-selectImage3.addEventListener("click", function(){
-  createThree();
-});

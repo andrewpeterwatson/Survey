be5b966e2aa7537a597a371c5f19182aa9129692
Andrew Watson <andrewpeterwatson@gmail.com>
2016-02-07 09:22:38 -0800

    additional work

--- survey.js
+++ survey.js
@@ -1,56 +1,64 @@
 var newProducts = [];
+var clickNum = 0;
+var images = ["bag", "banana", "boots", "chair", "cthulhu", "dragon", "pen", "scissors", "shark", "sweep", "unicorn", "usb", "water_can", "wine"];
+var picEl1 = document.getElementById("pic1");
+var picEl2 = document.getElementById("pic2");
+var picEl3 = document.getElementById("pic3");
+
 function Product(call,url) {
   this.call = call;
   this.url = "media/images/png/" + call + ".png";
   this.chosen = 0;
   this.viewed = 0;
 };
-var images = ["bag", "banana", "boots", "chair", "cthulhu", "dragon", "pen", "scissors", "shark", "sweep", "unicorn", "usb", "water_can", "wine"];
+
 populate = function() {
   for (calls in images) {
     newProducts.push(new Product(images[calls],this.call));
   }
 };
+
 randomNum = function(min, max) {
   return Math.floor(Math.random() * (images.length - 1) + 1);
 };
+
 tracker = function() {
-  for (prods in images) {
-    populate();
-    createThree = function() {
-      used = [];
-      notUsed = [];
-      newProd = [];
-      picEl1 = document.getElementById("pic1");
-      picEl2 = document.getElementById("pic2");
-      picEl3 = document.getElementById("pic3");
-        for (var i = 0; i < 3; i++) {
-          newProd.push(newProducts[randomNum()]);
-          // console.log(newProd);
-            // while (used.includes(newProd) === false) {
-            //   used.push(newProd);
-            //   console.log("FAIL");
-        // }
-      }
-      picEl1.src = newProd[0].url;
-      picEl2.src = newProd[1].url;
-      picEl3.src = newProd[2].url;
-    };
-    createThree();
-    var selectImage1 = document.getElementById("pic1");
-    var selectImage2 = document.getElementById("pic2");
-    var selectImage3 = document.getElementById("pic3");
-    selectImage1.addEventListener("click", function(){
-      createThree();
-    });
-    selectImage2.addEventListener("click", function(){
-      createThree();
-    });
-    selectImage3.addEventListener("click", function(){
-      createThree();
-    });
+  populate();
+  createThree = function() {
+    clickNum++;
+    console.log(clickNum);
+    this[randomNum].viewed++;
+    console.log(this[randomNum].viewed);
+    used = [];
+    notUsed = [];
+    newProd = [];
+      for (var i = 0; i < 3; i++) {
+        newProd.push(newProducts[randomNum()]);
+        // TODO: view counter not working
+        // console.log(newProd);
+          // while (used.includes(newProd) === false) {
+          //   used.push(newProd);
+          //   console.log("FAIL");
+      // }
+    }
+    picEl1.name = newProd[0].call;
+    picEl2.name = newProd[1].call;
+    picEl3.name = newProd[2].call;
+    picEl1.src = newProd[0].url;
+    picEl2.src = newProd[1].url;
+    picEl3.src = newProd[2].url;
+    createListeners();
   }
-  selectImage1.removeEventListener("click", function(){})
-  selectImage2.removeEventListener("click", function(){})
-  selectImage3.removeEventListener("click", function(){})
+  createThree();
+  // removeListeners();
+};
+createListeners = function() {
+  picEl1.addEventListener("click", createThree);
+  picEl2.addEventListener("click", createThree);
+  picEl3.addEventListener("click", createThree);
+};
+removeListeners = function() {
+  picEl1.removeEventListener("click", createThree);
+  picEl2.removeEventListener("click", createThree);
+  picEl3.removeEventListener("click", createThree);
 };

7aa64a7cc0a9b861601c7adb38df924b429df3ca
Andrew Watson <andrewpeterwatson@gmail.com>
2016-02-08 14:35:25 -0800

    choice validation

--- survey.js
+++ survey.js
@@ -1,9 +1,9 @@
 var newProducts = [];
 var clickNum = 0;
 var images = ["bag", "banana", "boots", "chair", "cthulhu", "dragon", "pen", "scissors", "shark", "sweep", "unicorn", "usb", "water_can", "wine"];
-// var picEl1 = document.getElementById("pic1");
-// var picEl2 = document.getElementById("pic2");
-// var picEl3 = document.getElementById("pic3");
+var picEl1 = document.getElementById("pic1");
+var picEl2 = document.getElementById("pic2");
+var picEl3 = document.getElementById("pic3");
 
 function Product(call,url) {
   this.call = call;
@@ -11,75 +11,68 @@ function Product(call,url) {
   this.chosen = 0;
   this.viewed = 0;
 };
-
 populate = function() {
   for (calls in images) {
     newProducts.push(new Product(images[calls],this.call));
   }
 };
-
-randomNum = function(min, max) {
-  return Math.floor(Math.random() * (images.length - 1) + 1);
-};
-
 populate();
-
-// createListeners();
-// removeListeners();
-
-var newProd = [];
-var tracker = function() {
-  for (var i = 0; i < 3; i++) {
-    newProd.push(newProducts[randomNum()])
-  }
-};
 var idx1 = 0;
 var idx2 = 0;
 var idx3 = 0;
-
 function getRandomImage() {
-  idx1 = Math.floor(Math.random()*newProducts.length);
+  console.log(clickNum);
+  clickNum++;
+  idx1 = Math.floor(Math.random() * newProducts.length);
   img1 = newProducts[idx1];
   idx2 = idx1;
   idx3 = idx1;
   while (idx3 === idx2 || idx2 === idx1 || idx3 === idx1) {
-    idx2 = Math.floor(Math.random()*newProducts.length);
-    idx3 = Math.floor(Math.random()*newProducts.length);
+    idx2 = Math.floor(Math.random() * newProducts.length);
+    idx3 = Math.floor(Math.random() * newProducts.length);
   }
   img2 = newProducts[idx2];
   img3 = newProducts[idx3];
-  document.getElementById('pic1').setAttribute('src', img1.url);
-  document.getElementById('pic2').setAttribute('src', img2.url);
-  document.getElementById('pic3').setAttribute('src', img3.url);
-}
-
-
-// picEl1.src = newProd[0].url;
-// picEl2.src = newProd[1].url;
-// picEl3.src = newProd[2].url;
-// // }
-
-// tracker = function() {
-//  for (var i = 0; i < 15; i++) {
-//     createThree = function() {
-//       clickNum++;
-//       console.log(clickNum);
-//       used = [];
-//       notUsed = [];
-//         for (var i = 0; i < 3; i++) {
-//           newProd.push(newProducts[randomNum()]);
-//       }
-//     createThree();
-//   }
-// };
-
-createListeners = function() {
-  picEl1.addEventListener("click", tracker);
-  picEl2.addEventListener("click", tracker);
-  picEl3.addEventListener("click", tracker);
+  picEl1.setAttribute('src', img1.url);
+  picEl2.setAttribute('src', img2.url);
+  picEl3.setAttribute('src', img3.url);
 };
+// createListeners = function() {
+//   picEl1.addEventListener("click", getRandomImage);
+//   picEl2.addEventListener("click", getRandomImage);
+//   picEl3.addEventListener("click", getRandomImage);
+// };
 removeListeners = function() {
-  picEl1.removeEventListener("click", tracker);
-  picEl2.removeEventListener("click", tracker);
-  picEl3.removeEventListener("click", tracker);
+  picEl1.removeEventListener("click", getRandomImage);
+  picEl2.removeEventListener("click", getRandomImage);
+  picEl3.removeEventListener("click", getRandomImage);
+};
+function clickFocus() {
+  document.getElementById("pic1").addEventListener("click", function(){
+    var choice = event.target.id;
+    console.log(choice);
+    if (choice === "pic1") {
+      var img = document.getElementById("pic1").getAttribute('src');
+      newProducts[idx1].chosen += 1;
+      console.log(newProducts[idx1].chosen);
+    }
+  });
+  document.getElementById("pic2").addEventListener("click", function(){
+    var choice = event.target.id;
+    console.log(choice);
+    if (choice === "pic2") {
+      var img = document.getElementById("pic2").getAttribute('src');
+      newProducts[idx2].chosen += 1;
+      console.log(newProducts[idx2].chosen);
+    }
+  });
+  document.getElementById("pic3").addEventListener("click", function(){
+    var choice = event.target.id;
+    console.log(choice);
+    if (choice === "pic3") {
+      var img = document.getElementById("pic3").getAttribute('src');
+      newProducts[idx3].chosen += 1;
+      console.log(newProducts[idx3].chosen);
+    }
+  });
 };
